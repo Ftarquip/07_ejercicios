2d5dea7072c31e94d5166fbbacc8498a2935148d
Ftarquip <ftarquip@unjbg.edu.pe>
2025-06-01 21:18:33 -0500

    ejercicio 3 suma funcional

--- ejercicio_03.cpp
+++ ejercicio_03.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
 using namespace std;
 //prototipo
-void fibonacci(int n);
+void fibonacci(int n,int &suma);
 
 int main() {
     int n, suma = 0;
+    cout << "Ingrese la cantidad de la serie: ";
     cin >> n;
 
-    fibonacci(n);
+    fibonacci(n, suma);
 
     cout << "Suma total: " << suma << endl;//aun no funcional
     return 0;
 }
 
-void fibonacci(int n) {
+void fibonacci(int n,int &suma) {
     int a = 0, b = 1, temp;
 
     for (int i = 0; i < n; i++) {
         cout << a << " ";
+        suma = suma + a;
         temp = a + b;
         a = b;
         b = temp;
     }
+    
     cout << endl;
 }

2c98bf79bb61c0de59e9e167a9926aafa9b3f124
Ftarquip <amidwar05@gmail.com>
2025-06-04 13:51:59 -0500

    mcdymcm

---
+++ mcdymcm_p.cpp
@@ -0,0 +1,27 @@
+//mcdymcm_p
+#include <iostream>
+using namespace std;
+void MCDyMCM(int x, int y, int *rMCD, int *rMCM);
+
+int main() {
+    int x = 18, y = 24;
+    int rMCD, rMCM;
+    MCDyMCM(x, y, &rMCD, &rMCM);
+    cout << "MCD: " << rMCD << endl;
+    cout << "MCM: " << rMCM << endl;
+    return 0;
+}
+void MCDyMCM(int a, int b, int *mcd, int *mcm) {
+    int i;
+    i = 1;
+    *mcd = 1;
+    while (i <= a && i <= b) {
+        if (a % i == 0 && b % i == 0) {
+            *mcd = i;
+        }
+        i++;
+    }
+    *mcm = (a * b) / (*mcd);
+    return;
+    
+}
